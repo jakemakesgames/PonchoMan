019421fb53f82cd39e20e14941bd26c98896e8f9
jakemakesgames <jakebrown.designer@gmail.com>
2018-10-28 20:30:08 +1100

    Base Shooting functionality implemented, needs cleaning up.

---
+++ Assets/OurAssets/Scripts/PonchoMan/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	[SerializeField] private float speed;
+	private Vector2 target;
+
+	void Start(){
+		// Set the target equal to the mousePosition
+		target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void Update(){
+		// Move the projectile towards the target
+		transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/OurAssets/Scripts/PonchoMan/SlingshotController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingshotController : MonoBehaviour {
+
+	[SerializeField] private GameObject projectile;
+	[SerializeField] private Transform shotPos;
+
+	void Update(){
+		// If the Left Mouse Button is being pressed, call the Fire function
+		if (Input.GetMouseButtonDown(0)) {
+			Fire ();
+		}
+	}
+
+	void Fire(){
+		Instantiate (projectile, shotPos.position, Quaternion.identity);
+	}
+}
